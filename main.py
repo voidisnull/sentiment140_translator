62cea333051167fbd62c2e0d91e7b445ab32937a
Saksham <voidisnull@duck.com>
2025-01-21 15:34:47 +0530

    Added rebase functionality

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ if __name__ == "__main__":
     with open("counter.txt", "r") as fin:
         count = int(fin.readline())
         count += 1
-    
+
+    # Rebasing
+    print("\nRebasing\n")
+    vc.rebase_from_upstream()
+
     # First pushing the counter value to main
     vc.update_counter_and_commit("counter.txt")
     vc.push_origin()
 
     # Starting the translating function
-    asyncio.run(translate.main(f"dataset/train_en/{count}.csv", f"dataset/train_hi/{count}.csv"))
+    asyncio.run(
+        translate.main(f"dataset/train_en/{count}.csv", f"dataset/train_hi/{count}.csv")
+    )
 
     # Pushing the translated dataset to main
+    print("\nPushing the changes to upstream\n")
     vc.add_dataset_chunk(count)
     vc.push_origin()
+
+    print("\nProcess Finished!!")
--- source/vc.py
+++ source/vc.py
@@ -2,6 +2,7 @@ import git
 
 repo = git.Repo("")
 
+
 def update_counter_and_commit(counterPath: str) -> None:
     """
     Updates the chunk counter and commits to the repo
@@ -28,9 +29,9 @@ def push_origin() -> None:
     """
     Pushes the repo to origin
     """
-    
+
     # Push changes to the 'main' branch on the 'origin' remote
-    origin = repo.remote(name = "origin")
+    origin = repo.remote(name="origin")
     origin.push(refspec="main:main")
 
 
@@ -42,10 +43,20 @@ def add_dataset_chunk(count: int, lang: str = "hi") -> None:
         count: Current chunk count
         lang: Language to which dataset has been translated
     """
-    
+
     # Add files to the staging area
     repo.git.add(A=True)
 
     # Commit the changes (if there are staged changes)
     if repo.is_dirty(untracked_files=True):
         repo.index.commit(f"{lang}_{count}.csv Added")
+
+
+def rebase_from_upstream():
+    """
+    Rebases the local repo from upstream
+    """
+
+    # Rebase from origin main
+    origin = repo.remote(name="origin")
+    origin.pull(refspec="main:main", rebase=True)
