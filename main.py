b3be87b2ee5b28e79497d11dc8089d9e5eb4907f
Saksham <voidisnull@duck.com>
2025-01-29 03:31:45 +0530

    Change manual git actions to github workflow

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ import asyncio
 from source import translate, vc
 
 if __name__ == "__main__":
-    # Rebasing
-    print("Rebasing\n")
-    vc.rebase_from_upstream()
-
     count: int = -1
 
-    # Loading completed count from the file
+    # Load completed count from the file
     with open("counter.txt", "r") as fin:
         line = fin.readline().strip()
         if line:
@@ -22,19 +18,9 @@ if __name__ == "__main__":
 
     print(f"Translating {count}th chunk\n")
 
-    # First pushing the counter value to main
-    vc.update_counter_and_commit("counter.txt")
-    vc.push_origin()
-
     # Starting the translating function
     asyncio.run(
         translate.main(f"dataset/train_en/{count}.csv", f"dataset/train_hi/{count}.csv")
     )
-
-    # Pushing the translated dataset to main
-    print("\nPushing the changes to upstream")
-    vc.rebase_from_upstream()
-    vc.add_dataset_chunk(count)
-    vc.push_origin()
 
     print("\nProcess Finished!!")
